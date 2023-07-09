718b057b1c17eb525e4309f2b92395f6e895a3f2
Amit Sheokand <amit.sheokand@tooli.qa>
2023-07-09 12:59:09 +0530

    initial setup

---
+++ src/main.rs
@@ -0,0 +1,63 @@
+use bevy::prelude::*;
+use bevy::window::WindowMode;
+use bevy_mod_picking::prelude::*;
+use bevy_panorbit_camera::{PanOrbitCamera, PanOrbitCameraPlugin};
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                mode: WindowMode::BorderlessFullscreen,
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_plugins(DefaultPickingPlugins)
+        .add_plugin(PanOrbitCameraPlugin)
+        .add_startup_system(setup)
+        .run();
+}
+
+/// set up a simple 3D scene
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn((
+        PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Plane::from_size(5.0))),
+            material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+            ..Default::default()
+        },
+        PickableBundle::default(),    // <- Makes the mesh pickable.
+        RaycastPickTarget::default(), // <- Needed for the raycast backend.
+    ));
+    commands.spawn((
+        PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+            material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+            transform: Transform::from_xyz(0.0, 0.5, 0.0),
+            ..Default::default()
+        },
+        PickableBundle::default(),    // <- Makes the mesh pickable.
+        RaycastPickTarget::default(), // <- Needed for the raycast backend.
+    ));
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..Default::default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, -4.0),
+        ..Default::default()
+    });
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_xyz(3.0, 3.0, 3.0).looking_at(Vec3::ZERO, Vec3::Y),
+            ..Default::default()
+        },
+        RaycastPickCamera::default(), // <- Enable picking for this camera
+        PanOrbitCamera::default(),
+    ));
+}
